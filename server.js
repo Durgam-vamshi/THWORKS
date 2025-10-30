212273df12e0cea19efa37f2bb71e9869a12b3b3
Durgam-vamshi <136604580+Durgam-vamshi@users.noreply.github.com>
2025-10-30 12:48:58 +0530

    Add health check endpoint to server

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.use("/api/insights", insightsRouter);
 
 app.use(errorHandler);
 
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
 app.listen(process.env.PORT, () =>
   console.log(` Server running on port ${process.env.PORT}`)
 );
